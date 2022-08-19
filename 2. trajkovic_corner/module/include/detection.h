b4f12702708296429dea155755079575a74bc688
JongRok Lee <99572329+JongRok-Lee@users.noreply.github.com>
2022-08-20 01:00:24 +0900

    Update detection.h

--- 2. trajkovic_corner/module/include/detection.h
+++ 2. trajkovic_corner/module/include/detection.h
@@ -8,11 +8,12 @@
 class Corner
 {
     public:
-        Corner(const std::string &path, double T1_, double T2_, std::string method_);
+        Corner(const std::string &path, double resize_param_, double T1_, double T2_, std::string method_);
         ~Corner();
     public:
         const std::string IA_method;
         const double T1, T2;
+        const double resize_param;
         cv::Mat img, src_img, resized_img;
         std::vector<std::vector<uchar>> src;
         std::vector<std::vector<uchar>> resized_src;
@@ -22,8 +23,7 @@ class Corner
         std::vector<int> size();
         std::vector<std::vector<double>> step1();
         std::vector<std::vector<double>> step2(std::vector<std::vector<double>>& candidates_);
-        std::vector<std::vector<double>> compute_R1();
-        std::vector<std::vector<double>> linear_compute_R2(std::vector<std::vector<double>> &candidates_);
-        std::vector<std::vector<double>> circular_compute_R2(std::vector<std::vector<double>> &candidates_);
+        std::vector<std::vector<double>> linear_compute_R(std::vector<std::vector<double>> &candidates_);
+        std::vector<std::vector<double>> circular_compute_R(std::vector<std::vector<double>> &candidates_);
 
 };
