454492e897931654bc7697a92783c3db69e5d32a
JongRok Lee <99572329+JongRok-Lee@users.noreply.github.com>
2022-08-20 01:00:04 +0900

    Update main.cpp

--- 2. trajkovic_corner/example/main.cpp
+++ 2. trajkovic_corner/example/main.cpp
@@ -3,13 +3,16 @@
 int main(int argc, char** argv)
 {   
     std::string image(argv[1]);
-    double T1 = atof(argv[2]), T2 = atof(argv[3]);
-    std::string method(argv[4]);
+    double resize_param = atof(argv[2]);
+    double T1 = atof(argv[3]),
+           T2 = atof(argv[4]);
+    std::string method(argv[5]);
+    
     std::cout << "Interpixel Approximation method: " << method << "\n";
     std::cout << "T1 : " << T1 << ",\t T2: " << T2 << "\n";
-    Corner Trajkovic(image, T1, T2, method);
-    Trajkovic.show();
 
+    Corner Trajkovic(image, resize_param, T1, T2, method);
+    Trajkovic.show();
 
     return 0;
 }
