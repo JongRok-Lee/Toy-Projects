1092665a843c3e7b9d7f8a6595a904ec0631d3a8
JongRok Lee <99572329+JongRok-Lee@users.noreply.github.com>
2022-09-16 23:53:49 +0900

    Update main.cpp

--- 3. Homography/example/main.cpp
+++ 3. Homography/example/main.cpp
@@ -1,27 +1,30 @@
 #include "module.h"
 
-namespace fs = std::experimental::filesystem;
-
 int main(int argc, char** argv)
 {   
     cv::Mat targetImg = cv::imread("sources/01.jpg", cv::IMREAD_ANYCOLOR);
     cv::Mat moveImg = cv::imread("sources/02.jpg", cv::IMREAD_ANYCOLOR);
 
-    int max_iter = atoi(argv[1]);
-    double resThr = atof(argv[2]);
+    const int max_iter = atoi(argv[1]);
+    const double resThr = atof(argv[2]);
     std::pair<std::vector<cv::Point2d>, std::vector<cv::Point2d>> ptsTM = get_pts(targetImg, moveImg);
     cv::Mat H1 = get_H(ptsTM);
-    cv::Mat H2 = DLT_H(ptsTM, max_iter, resThr);
+    std::pair<cv::Mat, cv::Mat> Hs = DLT_H(ptsTM, max_iter, resThr);
+    cv::Mat H2 = Hs.first, H3 = Hs.second;
     cv::Mat dst1 = H_transform(moveImg, H1);
     cv::Mat dst2 = H_transform(moveImg, H2);
+    cv::Mat dst3 = H_transform(moveImg, H3);
 
     std::cout<< "Homography (DLT + RANSAC): " << std::endl << H2 << std::endl;
+    std::cout<< "Homography (DLT + PROSAC): " << std::endl << H3 << std::endl;
     std::cout<< "Homography (OpenCV): " << std::endl << H1 << std::endl;
+    
 
     cv::imshow("targetImg", targetImg);
     cv::imshow("moveImg", moveImg);
     cv::imshow("OpenCV", dst1);
-    cv::imshow("JongRok", dst2);
+    cv::imshow("RANSAC", dst2);
+    cv::imshow("PROSAC", dst3);
     cv::waitKey(0);
     return 0;
 }
